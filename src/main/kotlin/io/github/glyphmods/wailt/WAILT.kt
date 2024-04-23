ca67b6dd2d83e55084858df326b4e01995927031
GingerIndustries <75683114+GingerIndustries@users.noreply.github.com>
2024-04-22 21:25:47 -0400

    Remove unused imports

--- src/main/kotlin/io/github/glyphmods/wailt/WAILT.kt
+++ src/main/kotlin/io/github/glyphmods/wailt/WAILT.kt
@@ -1,16 +1,9 @@
 package io.github.glyphmods.wailt
 
-import net.minecraft.client.Minecraft
-import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.Mod
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
-import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
-import thedarkcolour.kotlinforforge.neoforge.forge.runForDist
 
 @Mod(WAILT.ID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
