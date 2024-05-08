3673993b980b4d6b174135d861f3044f9e4d71fc
GingerIndustries <75683114+GingerIndustries@users.noreply.github.com>
2024-05-08 17:17:44 -0400

    Add Brick's toast texture

--- src/main/kotlin/io/github/glyphmods/wailt/SongToast.kt
+++ src/main/kotlin/io/github/glyphmods/wailt/SongToast.kt
@@ -6,15 +6,15 @@ import net.minecraft.client.gui.components.toasts.ToastComponent
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
 
-private val BACKGROUND_SPRITE = ResourceLocation("minecraft:toast/advancement")
+private val BACKGROUND_SPRITE = ResourceLocation("wailt:toast")
 
 class SongToast(private val artist: Component, private val track: Component) : Toast {
     override fun render(gui: GuiGraphics, component: ToastComponent, timeSinceLastVisible: Long): Toast.Visibility {
         val font = component.minecraft.font
         gui.blitSprite(BACKGROUND_SPRITE, 0, 0, width(), height())
 
-        gui.drawString(font, artist, 5, 7, 0xFFA500)
-        gui.drawString(font, track, 5, 18, 0xFFFFFF)
+        gui.drawString(font, track, 42, 5, 0xFFFFFF)
+        gui.drawString(font, artist, 42, 18, 0xFFA500)
 
         return if (timeSinceLastVisible < Config.toastDuration.get() * 1000 * component.notificationDisplayTimeMultiplier) {
             Toast.Visibility.SHOW
